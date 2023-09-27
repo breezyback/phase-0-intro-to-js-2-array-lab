2abf5c2e183d633f0df0d31178afef527b2f25ed
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-09-27 23:11:47 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = name => {
+    cats.push(name);
+};
+
+const destructivelyPrependCat = name => {
+    cats.unshift(name);
+};
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop();
+};
+
+const destructivelyRemoveFirstCat = () => {
+    cats.shift();
+};
+
+const appendCat = name => {
+    let newCats = [...cats]
+    newCats.push(name);
+    return newCats;
+};
+
+const prependCat = name => {
+    let newCats = [...cats]
+    newCats.unshift(name);
+    return newCats;
+};
+
+const removeLastCat = () => {
+    let newCats = [...cats]
+    newCats.pop();
+    return newCats;
+};
+
+const removeFirstCat = () => {
+    let newCats = [...cats]
+    newCats.shift();
+    return newCats;
+};
